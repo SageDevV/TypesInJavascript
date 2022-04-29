8b276e249014ff8a3a956ac8c6fc13ef529019a4
SageDevV <pandredbz@gmail.com>
2022-04-28 23:34:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Tipos do Javascript 
+//Operador const
+// Constantes, não há atribuições
+const string1 = 'name'; //string aspas simples
+const string2 = "name"; //string aspas duplas
+const string3 = `name`; //string com template
+const num = 5; //number
+const num2 = 2.5; //number
+const bool = true; //booleano
+const nullEx = null; //Null
+const arrayCollection = [1,2]; //Array
+const sym = Symbol("example"); //Identificador para propriedades de objetos
+
+//Operador let
+let tipo = "string"; //Atribuições dinâmicas
+let empty; //Undefined -> Não aponta para nenhum local em memória
+
+//Operador var
+var tipo1 = "string"; //Atribuições dinâmicas e re-declarações
+
